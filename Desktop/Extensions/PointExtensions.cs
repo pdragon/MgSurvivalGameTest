464a090f5860b95e640b599249632aa71fdcb3b2
f <mpa8@ya.ru>
2025-05-02 15:18:16 +0600

    PointExtension добавлен

---
+++ Desktop/Extensions/PointExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Utils;
+using System.Numerics;
+using System.Drawing;
+using XnaPoint = Microsoft.Xna.Framework.Point;
+
+namespace Desktop.Extensions
+{
+    public static class PointExtensions
+    {
+        /// <summary>
+        /// Преобразует System.Drawing.Point в Microsoft.Xna.Framework.Point.
+        /// </summary>
+        public static XnaPoint ToMonoGame(this Point v)
+        {
+            return new XnaPoint(v.X, v.Y);
+        }
+
+        public static Point ToSystemDrawing(this XnaPoint v)
+        {
+            return new Point(v.X, v.Y);
+        }
+    }
+}
